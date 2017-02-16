1ffab44e0881d4df90b1f6202fdc82603fea849d
ROXy <djeffik@gmail.com>
2017-02-16 17:23:56 +0200

    urls: style code

--- sixerr/urls.py
+++ sixerr/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from sixerrapp.views import home, gig_detail
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^social$/', include('social.apps.django_app.urls', namespace='social')),
