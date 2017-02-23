d1cd00372c24c7dcbaf61c038126358e61ed49e2
ROXy <djeffik@gmail.com>
2017-02-23 19:47:27 +0200

    add social_auth_pipeline

--- sixerr/settings.py
+++ sixerr/settings.py
@@ -135,6 +135,19 @@ AUTHENTICATION_BACKENDS = (
 
 LOGIN_REDIRECT_URL = '/'
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'sixerrapp.social_auth_pipeline.save_avatar',  # <--- set the path to the function
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
 SOCIAL_AUTH_FACEBOOK_KEY = '837112296438042'
 SOCIAL_AUTH_FACEBOOK_SECRET = 'f4e449ee98083dbf2630335233b3420a'
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
