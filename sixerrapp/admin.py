3bb8fdf4e667ab1b7c54e420ed98295dbf47ad59
ROXy <djeffik@gmail.com>
2017-02-24 20:50:17 +0200

    models

--- sixerrapp/admin.py
+++ sixerrapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Profile, Gig
+from .models import Profile, Gig, Purchase
 
 
 admin.site.register(Profile)
 admin.site.register(Gig)
+admin.site.register(Purchase)
 
