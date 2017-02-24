d512d877b74b917a8bdaaeb2ca73ee90187b6cc9
ROXy <djeffik@gmail.com>
2017-02-24 15:05:30 +0200

    urls: add profile url

--- sixerrapp/urls.py
+++ sixerrapp/urls.py
@@ -1,15 +1,12 @@
 from django.conf.urls import url
-from sixerrapp.views import \
-    home, \
-    gig_detail, \
-    create_gig, \
-    my_gigs, \
-    edit_gig
+from sixerrapp import views
+
 
 urlpatterns = [
-    url(r'^$', home, name='home'),
-    url(r'^gigs/(?P<id>[0-9]+)/$', gig_detail, name='gig_detail'),
-    url(r'^my_gigs/$', my_gigs, name='my_gigs'),
-    url(r'^create_gig/$', create_gig, name='create_gig'),
-    url(r'^edit_gig/(?P<id>[0-9]+)/$', edit_gig, name='edit_gig'),
+    url(r'^$', views.home, name='home'),
+    url(r'^gigs/(?P<id>[0-9]+)/$', views.gig_detail, name='gig_detail'),
+    url(r'^my_gigs/$', views.my_gigs, name='my_gigs'),
+    url(r'^create_gig/$', views.create_gig, name='create_gig'),
+    url(r'^edit_gig/(?P<id>[0-9]+)/$', views.edit_gig, name='edit_gig'),
+    url(r'^profile/(?P<username>\w+)/$', views.profile, name='profile'),
 ]
