b2ac8674b73290ac499b97337ce6ccab490bf837
ROXy <djeffik@gmail.com>
2017-02-24 15:51:01 +0200

    views: add form in profile view

--- sixerrapp/views.py
+++ sixerrapp/views.py
@@ -79,10 +79,16 @@ def my_gigs(request):
 
 @login_required(login_url='/')
 def profile(request, username):
-    try:
-        profile = Profile.objects.get(user__username=username)
-    except Profile.DoesNotExist:
-        return redirect('/')
+    if request.method == 'POST':
+        profile = Profile.objects.get(user=request.user)
+        profile.about = request.POST['about']
+        profile.slogan = request.POST['slogan']
+        profile.save()
+    else:
+        try:
+            profile = Profile.objects.get(user__username=username)
+        except Profile.DoesNotExist:
+            return redirect('/')
 
     gigs = Gig.objects.filter(user=profile.user, status=True)
     context = {
