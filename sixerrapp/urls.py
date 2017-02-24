95301296fa620f9837ec46a91123e55c12624aaf
ROXy <djeffik@gmail.com>
2017-02-25 01:53:11 +0200

    urls: add search url

--- sixerrapp/urls.py
+++ sixerrapp/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     url(r'^my_sellings/$', views.my_sellings, name='my_sellings'),
     url(r'^my_buyings/$', views.my_buyings, name='my_buyings'),
     url(r'^category/(?P<link>[\w|-]+)/$', views.category, name='category'),
+    url(r'^search/$', views.search, name='search'),
 ]
