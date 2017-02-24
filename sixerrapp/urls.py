cd69dfa5be5fb0617bac2faa959e38fbfc5517f0
ROXy <djeffik@gmail.com>
2017-02-24 09:24:41 +0200

    urls: fix url

--- sixerrapp/urls.py
+++ sixerrapp/urls.py
@@ -4,6 +4,6 @@ from sixerrapp.views import home, gig_detail, create_gig, my_gigs
 urlpatterns = [
     url(r'^$', home, name='home'),
     url(r'^gigs/(?P<id>[0-9]+)/$', gig_detail, name='gig_detail'),
-    url(r'^my_gigs/$', home, name='my_gigs'),
-    url(r'^create_gig/$', home, name='create_gig'),
+    url(r'^my_gigs/$', my_gigs, name='my_gigs'),
+    url(r'^create_gig/$', create_gig, name='create_gig'),
 ]
